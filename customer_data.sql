61739b06a1aae1db67641de3c2b58706dfa61bfd
rudra gaywala <rudra.gaywala277@gmail.com>
2026-03-30 23:31:51 +0530

    Add files via upload

---
+++ customer_data.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE customers (
+    CustomerID INT,
+    Name TEXT,
+    Age INT,
+    Gender TEXT,
+    Income INT,
+    Purchases INT,
+    Churn INT
+);
+
+INSERT INTO customers VALUES (1, 'Amit', 25, 'M', 40000, 5, 0);
+INSERT INTO customers VALUES (2, 'Neha', 30, 'F', 60000, 8, 1);
